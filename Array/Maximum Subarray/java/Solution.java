cb349a20485217c5244bcf54026c25bb69134355
Vimal raj R <vimalrajramalingam24@gmail.com>
2026-07-01 12:22:53 +0530

    feat: Add Maximum Subarray

--- Array/Maximum Subarray/java/Solution.java
+++ Array/Maximum Subarray/java/Solution.java
@@ -1,16 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-
-        int currentSum = nums[0];
-        int maxSum = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-
-            currentSum = Math.max(nums[i], currentSum + nums[i]);
-
-            maxSum = Math.max(maxSum, currentSum);
+        int max=nums[0];
+        for(int i=0;i<nums.length;i++){
+            int sum=0;
+            for(int j=i;j<nums.length;j++){
+                sum+=nums[j];
+                max=Math.max(sum,max);
+            }
         }
 
-        return maxSum;
+        return max;
     }
 }
