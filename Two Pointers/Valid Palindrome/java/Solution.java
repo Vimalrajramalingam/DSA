4e5138f5224e408123787e02b6c49e2e3c7c5f42
Vimal raj R <vimalrajramalingam24@gmail.com>
2026-07-01 10:18:33 +0530

    feat: Add Valid Palindrome

---
+++ Two Pointers/Valid Palindrome/java/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str=s.replaceAll("[^a-zA-Z]","").toLowerCase();
+        StringBuilder sb=new StringBuilder();
+        String sr=new StringBuilder(str).reverse().toString();
+        if(!str.equals(sr) ){
+            return false;
+        }
+        return true;
+    }
+}
