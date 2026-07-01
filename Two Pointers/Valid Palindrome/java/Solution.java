6c477e48f1802b00db63318725335b7bc2975b6d
Vimal raj R <vimalrajramalingam24@gmail.com>
2026-07-01 10:19:14 +0530

    feat: Add Valid Palindrome

--- Two Pointers/Valid Palindrome/java/Solution.java
+++ Two Pointers/Valid Palindrome/java/Solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String str=s.replaceAll("[^a-zA-Z]","").toLowerCase();
+        String str=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
         StringBuilder sb=new StringBuilder();
         String sr=new StringBuilder(str).reverse().toString();
         if(!str.equals(sr) ){
