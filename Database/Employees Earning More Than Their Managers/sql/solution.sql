3290997a17f0123fce1165e8d196ecdc680c4d09
Vimal raj R <vimalrajramalingam24@gmail.com>
2026-07-02 12:52:40 +0530

    feat: Add Employees Earning More Than Their Managers

--- Database/Employees Earning More Than Their Managers/sql/solution.sql
+++ Database/Employees Earning More Than Their Managers/sql/solution.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
-select e.name as Employee from Employee e
-join employee a
-on e.managerID=a.id
-where e.salary>a.salary ; 
+# Write your MySQL query statement below
+select e.name as Employee from Employee e
+join employee a
+on e.managerID=a.id
+where e.salary>a.salary ; 
