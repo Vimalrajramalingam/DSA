31d372707f6f031b2482574b9cd8a8c5cb5c649f
Vimal raj R <vimalrajramalingam24@gmail.com>
2026-08-06 14:29:42 +0530

    feat: Add Running Sum of 1d Array

--- LeetCode/Array/Running Sum of 1d Array/java/Solution.java
+++ LeetCode/Array/Running Sum of 1d Array/java/Solution.java
@@ -3,7 +3,7 @@ class Solution {
         int prefix[]=new int[nums.length];
         prefix[0]=nums[0];
         for(int i=1;i<nums.length;i++){
-            prefix[i]=prefix[i-1]+nums[i];
+            prefix[i]=nums[i]+prefix[i-1];
         }
         return prefix;
     }
