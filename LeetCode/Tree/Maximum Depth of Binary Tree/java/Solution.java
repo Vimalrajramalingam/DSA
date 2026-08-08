4b2be4e0ad08c7b49d78a2a82ccfd13ec75d116e
Vimal raj R <vimalrajramalingam24@gmail.com>
2026-08-08 14:27:23 +0530

    feat: Add Maximum Depth of Binary Tree

--- LeetCode/Tree/Maximum Depth of Binary Tree/java/Solution.java
+++ LeetCode/Tree/Maximum Depth of Binary Tree/java/Solution.java
@@ -15,12 +15,12 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-         if (root == null) {
+        if(root == null){
             return 0;
         }
-        int left_depth = maxDepth(root.left);
-        int right_depth = maxDepth(root.right);
-        return 1 + Math.max(left_depth, right_depth);
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return 1+Math.max(left,right);
+        
     }
 }
-    
