93bb04e736fc7bd7b6261ff5bead2110ff064b0c
Vimal raj R <vimalrajramalingam24@gmail.com>
2026-08-10 14:45:33 +0530

    feat: Add Valid Palindrome

--- LeetCode/Two Pointers/Valid Palindrome/java/Solution.java
+++ LeetCode/Two Pointers/Valid Palindrome/java/Solution.java
@@ -1,11 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String str=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
-        StringBuilder sb=new StringBuilder();
-        String sr=new StringBuilder(str).reverse().toString();
-        if(!str.equals(sr) ){
-            return false;
+        s=s.replaceAll("[^A-Za-z0-9]","");
+        s=s.toLowerCase();
+        int left = 0;
+        int right = s.length()-1;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
         }
-        return true;
+        return true ;
     }
 }
