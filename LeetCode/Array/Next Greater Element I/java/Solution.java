415844c0a7c3fb76cb3e9f1d43007b9c83b5fe54
Vimal raj R <vimalrajramalingam24@gmail.com>
2026-08-22 14:19:42 +0530

    feat: Add Next Greater Element I

--- LeetCode/Array/Next Greater Element I/java/Solution.java
+++ LeetCode/Array/Next Greater Element I/java/Solution.java
@@ -1,28 +1,26 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> nextGreaterMap = new HashMap<>();
-       
+        int n = nums2.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
         Stack<Integer> stack = new Stack<>();
-
-        
-        for (int num : nums2) {
-            
-            while (!stack.isEmpty() && num > stack.peek()) {
-
-                nextGreaterMap.put(stack.pop(), num);
+        int res[] = new int[nums1.length];
+        for(int i = n-1 ; i>=0 ; i--){
+            int current = nums2[i];
+            while(!stack.isEmpty() && stack.peek() <= current){
+                stack.pop();
             }
-            
-            stack.push(num);
+            if(stack.isEmpty()){
+                map.put(current , -1);
+            }
+            else{
+                map.put(current , stack.peek());
+            }
+            stack.push(current);
         }
-
-        
-        int[] result = new int[nums1.length];
-        
         for (int i = 0; i < nums1.length; i++) {
-           
-            result[i] = nextGreaterMap.getOrDefault(nums1[i], -1);
+            res[i] = map.get(nums1[i]);
         }
-
-        return result;
+        return res ;
+        
     }
 }
