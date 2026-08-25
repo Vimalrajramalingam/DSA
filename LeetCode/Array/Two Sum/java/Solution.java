6d11ca4d2d2abfe74bf602337ca80aeba866f5c9
Vimal raj R <vimalrajramalingam24@gmail.com>
2026-08-25 13:24:24 +0530

    feat: Add Two Sum

--- LeetCode/Array/Two Sum/java/Solution.java
+++ LeetCode/Array/Two Sum/java/Solution.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int index = 0;
-        for(int i = 0; i<nums.length ; i++){
-            int value = target - nums[i];
-            if(map.containsKey(value)){
-                return new int[] {map.get(value),i};
-            }
-            else{
-                map.put(nums[i],i);
+        int n = nums.length-1;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n+1;j++){
+                if(nums[i]+nums[j] == target){
+                    return new int[]{i,j};
+                }
+                
             }
         }
-        return new int[] {-1,-1};
+
+        return new int[]{-1,-1};
+        
     }
 }
