cce7c0c55a918d534db9981895e6ef10bc48b755
Vimal raj R <vimalrajramalingam24@gmail.com>
2026-08-25 15:04:54 +0530

    feat: Add Move Zeroes

--- LeetCode/Array/Move Zeroes/java/Solution.java
+++ LeetCode/Array/Move Zeroes/java/Solution.java
@@ -1,14 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int slow = 0;
-        for(int fast = 0;fast<nums.length;fast++){
-            if(nums[fast]!=0){
-                int temp=nums[slow];
-                nums[slow]=nums[fast];
-                nums[fast]=temp;
+        for(int i = 0;i<nums.length ; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[slow];
+                nums[slow] = temp;
                 slow++;
             }
         }
-       
     }
 }
