7842c61e98a460762046ea5216119d11a97f360c
Vimal raj R <vimalrajramalingam24@gmail.com>
2026-08-25 14:35:54 +0530

    feat: Add Maximum Subarray

--- LeetCode/Array/Maximum Subarray/java/Solution.java
+++ LeetCode/Array/Maximum Subarray/java/Solution.java
@@ -1,16 +1,11 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-
-        int currentSum = nums[0];
-        int maxSum = nums[0];
-
+        int cur = nums[0];
+        int max = nums[0];
         for (int i = 1; i < nums.length; i++) {
-
-            currentSum = Math.max(nums[i], currentSum + nums[i]);
-
-            maxSum = Math.max(maxSum, currentSum);
+            cur = Math.max(nums[i], cur + nums[i]);
+            max = Math.max(max, cur);
         }
-
-        return maxSum;
+        return max;
     }
 }
