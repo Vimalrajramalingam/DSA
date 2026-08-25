720c0a2d644a1da098cbe2c94772d83b22e29bb9
Vimal raj R <vimalrajramalingam24@gmail.com>
2026-08-25 14:54:21 +0530

    feat: Add Product of Array Except Self

--- LeetCode/Array/Product of Array Except Self/java/Solution.java
+++ LeetCode/Array/Product of Array Except Self/java/Solution.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
+        int n = nums.length ;
         int prefix[] = new int[n+1];
         int suffix[] = new int[n+1];
         int result[] = new int[n];
-        prefix[0]=1;
-        for(int i=0;i<n;i++){
+        prefix[0] = 1;
+        for(int i=0;i<nums.length;i++){
             prefix[i+1] = prefix[i]*nums[i];
         }
-        suffix[n] = 1;
-        for(int i=n-1;i>=0;i--){
+        suffix[nums.length] = 1;
+        for(int i = nums.length-1;i>=0;i--){
             suffix[i] = suffix[i+1]*nums[i];
         }
-        for(int i = 0 ; i < n ; i++){
+        for(int i = 0;i<nums.length;i++){
             result[i] = prefix[i]*suffix[i+1];
         }
-        return result;
+        return result ;
     }
 }
