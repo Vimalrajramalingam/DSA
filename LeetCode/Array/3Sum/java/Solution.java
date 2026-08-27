4446b8f69bc07c8c6cfd709d0024e68b13b8bcc3
Vimal raj R <vimalrajramalingam24@gmail.com>
2026-08-27 09:27:48 +0530

    feat(leetcode): Sync Binary Tree Right Side View and 12 others

--- LeetCode/Array/3Sum/java/Solution.java
+++ LeetCode/Array/3Sum/java/Solution.java
@@ -1,31 +1,57 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> res = new ArrayList<>();
+
         Arrays.sort(nums);
-        int n = nums.length;
-        List<List<Integer>> ans = new ArrayList<>();
 
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int i = 0; i < nums.length - 2; i++) {
 
-            int left = i + 1, right = n - 1;
+            // Skip duplicate first elements
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
 
             while (left < right) {
+
                 int sum = nums[i] + nums[left] + nums[right];
 
                 if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    res.add(Arrays.asList(
+                        nums[i],
+                        nums[left],
+                        nums[right]
+                    ));
+
                     left++;
                     right--;
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-                } else if (sum < 0) {
+
+                    // Skip duplicate left values
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    // Skip duplicate right values
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+
+                }
+                else if (sum < 0) {
                     left++;
-                } else {
+                }
+                else {
                     right--;
                 }
             }
         }
 
-        return ans;
+        return res;
     }
 }
